c90db7d7067facd8a1b8f52e6c4214255d828507
Florindo López <73351778+florindofp@users.noreply.github.com>
2025-05-14 12:16:47 +0200

    Add files via upload

---
+++ clima.js
@@ -0,0 +1,14 @@
+
+
+var clima = {};
+fetch('./js/current_clima.json')
+    .then((response) => response.json())
+    .then((json) => fillContent(json));
+
+function fillContent(clima){
+    var hours = clima.forecast.forecastday.hour;
+    
+    hours.forEach(function(item) {
+    
+    })
+}
